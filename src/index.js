968baef46add518ae51816662d9ed679c1dd8c97
Tommi <tommi.vainio21@gmail.com>
2021-02-22 18:45:32 +0200

    Sodexo menu improved, app can now check distance to campuses

--- src/index.js
+++ src/index.js
@@ -1,40 +1,63 @@
 import SodexoData from './modules/sodexo-data';
 import FazerData from './modules/fazer-data';
+import {getLocation, getDistance} from './modules/calculate-distance';
 
-const menuContainer = document.querySelector('.restaurant');
+const resContainer = document.querySelector('.restaurant');
 const restaurants = [{
   displayName: 'Myyrmäen Sodexo',
   name: 'myyrmaki',
   id: 152,
+  lat: 60.2586191,
+  long: 24.8432836,
   type: SodexoData
 }, {
   displayName: 'Karaportin Fazer Food & Co',
   name: 'karamalmi',
   id: 270540,
+  lat: 60.2238794,
+  long: 24.7559603,
   type: FazerData
 }, {
   displayName: 'Myllypuron Sodexo',
   name: 'myllypuro',
   id: 158,
+  lat: 60.2236145,
+  long: 25.0761622,
   type: SodexoData
 }, {
   displayName: 'Arabian Sodexo',
   name: 'arabia',
   id: 158,
+  lat: 60.2103774,
+  long: 24.9788837,
   type: SodexoData
 }];
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 
 const today = new Date().toISOString().split('T')[0];
 let currentCampus = 'myllypuro';
 let language = 'fi';
 
 const getMenu = async () =>{
+  console.log('Toimii');
   for (const restaurant of restaurants) {
     if(restaurant.name === currentCampus){
       try {
           const parsedMenu = await restaurant.type.getDailyMenu(restaurant.id, language, today);
-          renderMenu(parsedMenu, restaurant.displayName);
-
+          if(restaurant.type === SodexoData){
+            renderSodexoMenu(parsedMenu, restaurant.displayName);
+          } else{
+            renderMenu(parsedMenu, restaurant.displayName);
+          }
       } catch (error) {
         console.error(error);
         let message;
@@ -50,7 +73,7 @@ const getMenu = async () =>{
 };
 
 const renderMenu = (data, name) => {
-  menuContainer.innerHTML = '';
+  resContainer.innerHTML = '';
   const restaurantName = '<h3>' + name + '</h3>';
   const ul = document.createElement('ul');
   for (const item of data) {
@@ -58,15 +81,64 @@ const renderMenu = (data, name) => {
     listItem.textContent = item;
     ul.appendChild(listItem);
   }
-  menuContainer.innerHTML += restaurantName;
-  menuContainer.appendChild(ul);
+  resContainer.innerHTML += restaurantName;
+  resContainer.appendChild(ul);
+};
+
+const renderSodexoMenu = (data, name) => {
+  resContainer.innerHTML = '';
+  const restaurantName = '<h3>' + name + '</h3>';
+  const menuContainer = document.createElement('div');
+  menuContainer.classList.add('menu');
+  for (const item of data) {
+    const mealContainer = document.createElement('div');
+    mealContainer.classList.add('menuItem');
+
+    const nameContainer = document.createElement('div');
+    nameContainer.classList.add('menuName');
+    nameContainer.innerHTML = '<h4>' + item.category + '</h4>';
+    nameContainer.innerHTML += '<p>' + item.title + '</p>';
+
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('mealInfo');
+
+    const priceContainer = document.createElement('div');
+    priceContainer.classList.add('menuPrice');
+    priceContainer.innerHTML = '<br><p>' + item.price + '</p>';
+
+    const codeContainer = document.createElement('div');
+    codeContainer.classList.add('menuCode');
+    codeContainer.innerHTML = '<br><p>' + item.code + '</p>';
+
+    infoContainer.appendChild(priceContainer);
+    infoContainer.appendChild(codeContainer);
+    mealContainer.appendChild(nameContainer);
+    mealContainer.appendChild(infoContainer);
+    menuContainer.appendChild(mealContainer);
+  }
+  resContainer.innerHTML += restaurantName;
+  resContainer.appendChild(menuContainer);
 };
 
 const NoMenuFoundNotification = (message, name) => {
   const restaurantName = '<h3>' + name + '</h3>';
   const noMenuMessage = `<p>${message}</p>`;
-  menuContainer.innerHTML += restaurantName;
-  menuContainer.innerHTML += noMenuMessage;
+  resContainer.innerHTML += restaurantName;
+  resContainer.innerHTML += noMenuMessage;
 };
 
+const nearestCampus = () => {
+  getLocation()
+  .then((position) => {
+    const currentLatitude = position.coords.latitude;
+    const currentLongitude = position.coords.longitude;
+    const distance = getDistance(currentLatitude, currentLongitude, restaurants[1].lat, restaurants[1].long, 'K');
+    alert('Dis: ' + distance);
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+};
+
+nearestCampus();
 getMenu();
--- src/modules/sodexo-data.js
+++ src/modules/sodexo-data.js
@@ -1,33 +1,35 @@
 import {getMenus} from "./network";
 const sodexoAddress = 'https://www.sodexo.fi/ruokalistat/output/daily_json';
-let coursesEn = [];
-let coursesFi = [];
+let menu = [];
 
 const parseSodexoMenu = (data, lang) => {
   console.log('SodexoData: ' + data.courses);
   const courses = Object.values(data.courses);
-  coursesEn = [];
-  coursesFi = [];
+  menu = [];
   for(const course of courses) {
-    coursesEn.push(course.title_en);
-    coursesFi.push(course.title_fi);
+    let menuItem = {};
+    menuItem.category = course.category;
+    if(lang === 'fi'){
+      menuItem.title = course.title_fi;
+    } else{
+      menuItem.title = course.title_en;
+    }
+    menuItem.price = course.price;
+    menuItem.code = course.dietcodes;
+    menu.push(menuItem);
   }
-  if(lang === 'fi'){
-    return coursesFi;
-  }else{
-    return coursesEn;
-  }
+    return menu;
 };
 
 const getDailyMenu = async (restaurantId, lang, date) => {
   let menuData;
   try {
     menuData = await getMenus(`${sodexoAddress}/${restaurantId}/${date}`);
-    await console.log('Menu: ' + JSON.stringify(menuData));
   } catch (error) {
     throw new Error(error.message);
   }
   const parsedMenu = parseSodexoMenu(menuData, lang);
+  console.log('Parsed: ' + JSON.stringify(parsedMenu));
   return parsedMenu;
 };
 
